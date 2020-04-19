6d20ca9cb11a303fa20092515140d2b723ba302a
liuhao326 <326047736@qq.com>
2020-04-19 20:45:48 +0800

    完善目录换行问题

--- TOC.py
+++ TOC.py
@@ -7,18 +7,18 @@ lines_in_file = []
 """生成目录列表中的某一项"""
 def creat_directory_line(line,headline_mark):
     if headline_mark == '#':
-        return '[' + line[2:-1] + '](#' + line[2:-1] + ')\n'
+        return '[' + line[2:-1] + '](#' + line[2:-1] + ")\n\n"
     elif headline_mark == '##':
         #&emsp;为Markdown中的一种缩进，这里不直接用空格作为缩进是因为多个空格一起出现可能会生成代码块，引发歧义
-        return '&emsp;[' + line[3:-1] + '](#' + line[3:-1] + ')\n'
+        return '&emsp;[' + line[3:-1] + '](#' + line[3:-1] + ")\n\n"
     elif headline_mark == '###':
-        return '&emsp;&emsp;[' + line[4:-1] + '](#' + line[4:-1] + ')\n'
+        return '&emsp;&emsp;[' + line[4:-1] + '](#' + line[4:-1] + ")\n\n"
     elif headline_mark == '####':
-        return '&emsp;&emsp;&emsp;[' + line[5:-1] + '](#' + line[5:-1] + ')\n'
+        return '&emsp;&emsp;&emsp;[' + line[5:-1] + '](#' + line[5:-1] + ")\n\n"
     elif headline_mark == '#####':
-        return '&emsp;&emsp;&emsp;&emsp;[' + line[6:-1] + '](#' + line[6:-1] + ')\n'
+        return '&emsp;&emsp;&emsp;&emsp;[' + line[6:-1] + '](#' + line[6:-1] + ")\n\n"
     elif headline_mark == '######':
-        return '&emsp;&emsp;&emsp;&emsp;&emsp;[' + line[7:-1] + '](#' + line[7:-1] + ')\n'
+        return '&emsp;&emsp;&emsp;&emsp;&emsp;[' + line[7:-1] + '](#' + line[7:-1] + ")\n\n"
 
 """生成目录列表"""
 def creat_directory(f):
