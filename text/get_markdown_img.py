7895c891a939029c6327030bb25c581d1318f875
liuhao <liuhao@zay.com>
2024-08-23 21:33:34 +0800

    update get_markdown_img.py

--- text/get_markdown_img.py
+++ text/get_markdown_img.py
@@ -1,5 +1,6 @@
 import os
 import re
+import uuid
 import requests
 
 """
@@ -16,21 +17,24 @@ def download_images(md_file):
         md_content = f.read()
 
     # 匹配图片链接
-    pattern = r"!\[.*?\]\((https?://.*?\.(?:png|jpg|jpeg|gif))\)"
+    pattern = r"!\[.*?\]\((https?://.*)\)"
     image_urls = re.findall(pattern, md_content)
-
     # 下载图片并替换链接
+    print('找到' + str(len(image_urls)) + '张图片')
     for url in image_urls:
         response = requests.get(url)
         if response.status_code == 200:
             # 获取文件名
             filename = os.path.basename(url)
+            if not filename:
+                filename = str(uuid.uuid4()) + ".jpg"
             # 保存图片到本地
             with open(os.path.join(dir_name, filename), "wb") as f:
                 f.write(response.content)
             # 替换 Markdown 文件中的链接
             md_content = md_content.replace(url, f"./{dir_name}/{filename}")
-
+        else:
+            print("下载图片失败：" + url)
     # 保存更新后的 Markdown 文件
     with open(md_file, "w", encoding="utf-8") as f:
         f.write(md_content)
